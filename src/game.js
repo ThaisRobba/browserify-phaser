5a49ab4c9810cd894481691aaeeb736187ca9432
OttoRobba <ottorobba@gmail.com>
2014-12-09 23:06:22 -0200

    ;

--- src/game.js
+++ src/game.js
@@ -4,7 +4,7 @@ window.game = new Phaser.Game(800, 600, Phaser.AUTO);
 game.globals = {
     //Add variables here that you want to access globally
     //score: 0 could be accessed as game.globals.score for example
-}
+};
 
 game.state.add('play', require('./states/play.js'));
 game.state.add('load', require('./states/load.js'));
